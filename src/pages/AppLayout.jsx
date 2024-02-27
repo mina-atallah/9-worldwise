39158c80d56d4064b83177ffb43d60f2fd9bb7f0
mina-atallah <minaatallah18@gmail.com>
2024-02-27 22:39:58 +0200

    Build App Pages & Used CSS Modules for Styling

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <h3>Navigate</h3>
+    </div>
+  );
+}
+
+export default AppLayout;
