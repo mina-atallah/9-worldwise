b5f071c352eed9a6f6f67fa9f193e0546076d3e2
mina-atallah <minaatallah18@gmail.com>
2024-02-28 18:49:01 +0200

    Nested Routes  - created nested routes in AppLayout omponent  - used THE Outlet component  inside the Sidebar component to display the nested routes components  - used index route to display the default child route

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,14 @@ import AppNav from "./AppNav";
 import Logo from "./Logo";
 import styles from "./Sidebar.module.css";
 import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of Cities</p>
+      <Outlet />
       <Footer />
     </div>
   );
