05b9d84cf46ca83ceedbf2cb3cb09a9ffeb9230b
mina-atallah <minaatallah18@gmail.com>
2024-03-10 07:37:15 +0200

    Setting Map Position with Geolocation - created a new custom hook useGeolocation to get user position when clicking on a button - synchronized the map position with the use current position using useEffect hook

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -8,8 +8,10 @@ import {
   useMap,
   useMapEvents,
 } from "react-leaflet";
-import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
+import styles from "./Map.module.css";
 
 function Map() {
   const { cities } = useCities();
@@ -19,6 +21,11 @@ function Map() {
     - it should be a state so when the postion changes we need the map to re-render
   */
   const [mapPosition, setMapPosition] = useState([40, 0]);
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
 
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
@@ -30,8 +37,21 @@ function Map() {
     },
     [mapLat, mapLng]
   );
+  // to synchronize the mapPosition with the Position coming from the useGeoLocation hook
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "loading..." : "use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={6}
