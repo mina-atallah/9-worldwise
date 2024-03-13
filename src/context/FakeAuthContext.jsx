e7ff2aeb660eda8dfa4ee1fd34ab872abb48a8ef
mina-atallah <minaatallah18@gmail.com>
2024-03-13 13:52:18 +0200

    Adding a Fake Authentication - 1st Step - created a context compnenet for  the authentication to prodcast it's state to the componenets tree - used useReducer for the authentication state - used a hard coded user object to ckeck if data is valid to perform the login

---
+++ src/context/FakeAuthContext.jsx
@@ -0,0 +1,59 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      // merging the state because if needed to add one more peice state we don't go back and add it here
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      // you can return the initial state
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("Unkown Action");
+  }
+}
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  // functions are kept here because in real life they made API calls
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider value={(user, isAuthenticated, login, logout)}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("Context was outside AuthProvider");
+}
+
+export { AuthProvider, useAuth };
